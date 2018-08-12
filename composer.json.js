af969a1d62e09eb12e36a25089bef6eb76427ff7
libaocai <oracle_lee@qq.com>
2018-08-12 13:50:54 +0800

    add composer.json

---
+++ composer.json.js
@@ -0,0 +1,24 @@
+{
+    "name": "libaocai1990/sourlemon-helper",
+    "description": "sourlemon helper",
+    "type": "sourlemon-helper",
+    "keywords": ["sourlemon","helper","extension"],
+    "license": "MIT",
+    "authors": [
+        {
+            "name": "libaocai",
+            "email": "oracle_lee@qq.com"
+        }
+    ],
+    "require": {
+         "php": ">=5.4"
+    },
+    "autoload": {
+        "files": [
+            "src/Helper.php"
+        ],
+        "psr-4": {
+            "libaocai1990\\sourlemon-helper\\": "src/"
+        }
+    }
+}
